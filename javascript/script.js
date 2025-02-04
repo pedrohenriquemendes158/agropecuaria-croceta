5fe040369a16c342acb52cfd056a20f318379b19
Pedro Henrique <henriquemendesp85@gmail.com>
2025-02-04 16:55:49 -0300

    atualizado

--- javascript/script.js
+++ javascript/script.js
@@ -1,27 +1,12 @@
+document.getElementById('destaque-img').addEventListener('click', () =>{
+    document.getElementById('inicio').style.display = 'none';
+    document.getElementById('catalogo').style.display = 'block'
 
+});
 
-    let currentindex = 0;
-    const carrosselContainer = document.querySelector('.carrossel-container');
-    const totalitems = document.querySelectorAll('carrossel-item').length;
+document.getElementById('voltar-inicio').addEventListener('click', () =>{
+    document.getElementById('catalogo').style.display = 'none';
+    document.getElementById('inicio').style.display = 'block'
 
-    document.querySelector('.prev').addEventListener('click',() =>{
-        if (currentindex > 0) {
-            currentindex--;
-        } else {
-                currentindex = totalitems - 1;
-        }
-        updateCarrossel();
-    });
+});
 
-        document.querySelector('.next').addEventListener('click', () =>{
-            if (currentindex < totalitems -1) {
-                currentindex++;
-            } else {
-                currentindex = 0;
-            }
-        updateCarrossel();
-    });
-    function updateCarrossel() {
-        const offset = -currentindex * 100;
-        carrosselContainer.style.transform = `translateX(${offset}%)`;
-    }   
