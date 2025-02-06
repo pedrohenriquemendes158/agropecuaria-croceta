e201a12e65336310c6af9772a8fd46176f4ee114
Pedro Henrique <henriquemendesp85@gmail.com>
2025-02-06 18:20:01 -0300

    finish

--- javascript/script.js
+++ javascript/script.js
@@ -9,3 +9,11 @@ document.getElementById('voltar-inicio').addEventListener('click', () =>{
     document.getElementById('inicio').style.display = 'block'
 
 });
+
+function carregarDetalhes(imagem,titulo,descricao,teor,notas){
+    document.getElementById('modalImagem').src = imagem;
+    document.getElementById('modalTitulo').innerText = titulo;
+    document.getElementById('modalDescricao').innerText = descricao;
+    document.getElementById('modalTeor').innerText = teor;
+    document.getElementById('modalNotas').innerText = notas;
+}
